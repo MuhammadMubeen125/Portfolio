e7c4a3ea217ef2a9d56d1281c6955b644399542d
Mubeen <mubeensoomro2002@gmail.com>
2024-11-14 14:26:19 +0000

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
       <main className="flex min-h-screen flex-col items-center bg-[#121212]"> 
-      {/* updated */}
+      
       </main>
    
   );
